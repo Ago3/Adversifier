50e6d00bab3f96ab386936a6b08d97d41abe35e6
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2020-07-29 14:14:08 +0200

    Update info

--- info/info.py
+++ info/info.py
@@ -1,7 +1,7 @@
 CONFIG_DIR = 'config'
 CONFIG_FILE = '/'.join([CONFIG_DIR, 'example.json'])  # CUSTOMIZE
 DATA = 'DATA'
-DATASET = '/'.join([DATA, 'waseem.tsv'])  # CUSTOMIZE
+DATASET = '/'.join([DATA, 'waseem_0.tsv'])  # CUSTOMIZE
 ORG = '/'.join([DATA, 'org.tsv'])  # CUSTOMIZE
 CACHE_DIR = 'CACHE'
 LEXICON = '/'.join([DATA, 'hurtlex_EN_1.2.tsv'])
