21466eda2a398568d06196a88219863043cb3552
Ago3 <agostina.calabrese3@gmail.com>
2021-02-01 10:27:58 +0000

    Fix import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .files import get_config, get_high_corr_words, get_hateful_words
-from .math import geometric_mean, setting_f1_score, is_significant
+from .math import geometric_mean, setting_score, is_significant
 from .twitter import preprocess_tweet
