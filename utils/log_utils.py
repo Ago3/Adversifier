32f5897cc1915337ab70e3bbd69755316a3a9cdd
Ago3 <agostina.calabrese3@gmail.com>
2021-02-08 15:00:38 +0000

    Use log file

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -4,6 +4,6 @@ import os
 
 def log(model_name, scores):
 	if not os.path.exists(LOG):
-		os.mkdirs(LOG)
+		os.mkdir(LOG)
 	with open(RES_FILE, 'a+') as f:
 		f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(model_name, scores['f1_o'], scores['quoting_a_to_n'], scores['corr_n_to_n'], scores['flip_n_to_a'], scores['corr_a_to_a'], scores['aaa'], scores['hashtag_check']))
