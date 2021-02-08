c50825f159f0d6efff7844b23275ee087522c6ab
Ago3 <agostina.calabrese3@gmail.com>
2021-02-08 16:24:15 +0000

    Improve logging

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -5,6 +5,6 @@ import os
 def log(model_name, results):
 	if not os.path.exists(LOG):
 		os.mkdir(LOG)
-	scores = {k : v * 100 for k, v in results.items()}
+	scores = {k : "%.2f" % (v * 100) for k, v in results.items()}
 	with open(RES_FILE, 'a+') as f:
-		f.write('{}\t{0:.2f}\t{0:.2f}\t{0:.2f}\t{0:.2f}\t{0:.2f}\t{0:.2f}\t{0:.2f}\n'.format(model_name, scores['f1_o'], scores['quoting_a_to_n'], scores['corr_n_to_n'], scores['flip_n_to_a'], scores['corr_a_to_a'], scores['aaa'], scores['hashtag_check']))
+		f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(model_name, scores['f1_o'], scores['quoting_a_to_n'], scores['corr_n_to_n'], scores['flip_n_to_a'], scores['corr_a_to_a'], scores['aaa'], scores['hashtag_check']))
