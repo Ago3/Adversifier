67260cfb3e591124a88528b75d0991f1f5669838
Ago3 <agostina.calabrese3@gmail.com>
2021-02-09 10:40:51 +0000

    Integrate HateCheck Counter Quote

--- utils/hatecheck_utils.py
+++ utils/hatecheck_utils.py
@@ -3,17 +3,17 @@ import numpy as np
 
 
 def get_counter_quote_instances():
-	examples = list()
-	with open(HATECHECK_F20, 'r') as f:
-		for line in f.readlines():
-			examples.append(line.split('\t')[1].strip())
-	return examples
+    examples = list()
+    with open(HATECHECK_F20, 'r') as f:
+        for line in f.readlines():
+            examples.append(line.split('\t')[1].strip())
+    return examples
 
 
 def evaluate_on_hatecheck_counter_quote(predictor):
-	examples = get_counter_quote_instances()
-	predictions = predictor([examples])
-	labels = np.zeros((len(examples),))
-	tp = (np.array(predictions) == np.array(labels)).sum()
+    examples = get_counter_quote_instances()
+    predictions = predictor([examples])
+    labels = np.zeros((len(examples),))
+    tp = (np.array(predictions) == np.array(labels)).sum()
     tpr = tp / len(predictions)
     print('TPR on HateCheck counter quote: ', tpr)
