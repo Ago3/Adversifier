a9f9f7be5b7594df4c87d54c006266a8186bdf00
Ago3 <agostina.calabrese3@gmail.com>
2021-02-18 13:10:38 +0000

    Implement get_waseem_data

--- utils/waseem_dataset_utils.py
+++ utils/waseem_dataset_utils.py
@@ -9,11 +9,12 @@ def get_waseem_data():
     data = dict()
     for filename, dataset_name in zip(files, ['train', 'test']):
         with open(filename, 'r') as f:
-            split_ids = [line.strip() for line in f.readlines()]
-            split_posts = [id2post[idx] for idx in split_ids if idx in id2post ]
-            split_labels = [LABELS.index(id2label[idx]) for idx in split_ids if idx in id2label]
-            if len(split_ids) > len(split_posts):
-                print('Warning: {} tweets missing from {} set'.format(len(split_ids) - len(split_posts), dataset_name))
+            lines = f.readlines()
+            split_ids = [line.strip() for line in lines if line.strip() in id2post]
+            split_posts = [id2post[idx] for idx in split_ids]
+            split_labels = [LABELS.index(id2label[idx]) for idx in split_ids]
+            if len(lines) > len(split_posts):
+                print('Warning: {} tweets missing from {} set'.format(len(lines) - len(split_posts), dataset_name))
             assert len(split_posts) == len(split_labels), 'Posts and labels should be in same number'
             split_binary_labels = [l if l <= 1 else 1 for l in split_labels]
             extra_info_the_model_might_need = ['' for l in split_binary_labels]  # you can use this variable to pass, e.g., conversation context
@@ -22,16 +23,14 @@ def get_waseem_data():
 
 
 def __read_waseem_tsv_file__():
-    with open(WASEEM_TSV_FILE, 'r') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter='\t')
-        next(csvreader)  # Skip header
+    with open(WASEEM_TSV_FILE, 'r') as f:
         id2post = dict()
         id2label = dict()
-        # data = dict()
-        for line in csvreader:
-            id_example = line[0]
-            tweet = line[1]
-            label = line[2]
+        for line in f.readlines()[1:]:  # Skip header
+            fields = line.split('\t')
+            id_example = fields[0].strip()
+            tweet = fields[1].strip()
+            label = fields[2].strip()
             id2post[id_example] = tweet
             id2label[id_example] = label
         return id2post, id2label
