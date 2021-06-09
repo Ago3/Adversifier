bf55e54b8386a118adb7a55a918fa5eead217e4d
Ago3 <agostina.calabrese3@gmail.com>
2021-06-09 14:34:15 +0100

    Update math utils

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -27,40 +27,11 @@ def setting_score(predictions, labels, setting_name):
     return tpr
 
 
-# def old_is_significant(mean_score, new_score, eps=0.00001):
-#     # mean_score = 0.1
-#     # new_score = 0.0
-#     mean_score += eps
-#     new_score += eps
-#     statistic = chisquare([new_score], [mean_score])[0]
-#     # print('Statistic: ', statistic)
-#     critical_value = 3.84
-#     return statistic > critical_value
-
-
 def is_significant(expected_scores, observed_scores, p_value_ref=0.05, threshold=0.05):
-    print(expected_scores, observed_scores)
-    # expected_scores = [700, 500, 20000000, 30000000]
-    # observed_scores = [680, 520, 20000001, 29999999]
-    # chi, p_value = chisquare(observed_scores, f_exp=expected_scores, ddof=1)
-    # diff = max([abs(e - o) / e for e, o in zip(expected_scores, observed_scores)])
+    expected_scores *= 100
+    observed_scores *= 100
     diff = abs(expected_scores - observed_scores) / expected_scores
-    # print(chi, p_value, diff)
     std = expected_scores * 0.2
     zscore = (observed_scores - expected_scores) / std
-    p_value = norm.sf(abs(z_score)) * 2
-    print(p_value, diff)
-    print(p_value <= p_value_ref, diff >= threshold)
-    # expected_scores = [700, 500, 20, 30]
-    # observed_scores = [680, 520, 21, 29]
-    # chi, p_value = chisquare(observed_scores, f_exp=expected_scores)
-    # diff = max([abs(e - o) / e for e, o in zip(expected_scores, observed_scores)])
-    # print(chi, p_value, diff)
-    # print(p_value <= p_value_ref, diff >= threshold)
-    # expected_scores = [1900, 3000]
-    # observed_scores = [2000, 2900]
-    # chi, p_value = chisquare(observed_scores, f_exp=expected_scores)
-    # diff = max([abs(e - o) / e for e, o in zip(expected_scores, observed_scores)])
-    # print(chi, p_value, diff)
-    # print(p_value <= p_value_ref, diff >= threshold)
+    p_value = norm.sf(abs(zscore)) * 2
     return p_value <= p_value_ref and diff >= threshold
