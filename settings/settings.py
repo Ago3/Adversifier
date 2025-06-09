c7dd5905f1bb8332452bbb1e01f47047e2a8589a
Ago3 <agostina@Agostinas-MacBook-Pro.local>
2025-06-09 13:08:00 +0100

    Upadating library versions

--- settings/settings.py
+++ settings/settings.py
@@ -3,6 +3,15 @@ import random
 import re
 from utils import get_high_corr_words, get_hateful_words
 import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+    
 nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 from info import TEMPLATES
--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-pip3 install nltk sklearn numpy pickle requests
+conda create -n aaa python=3.8
+conda activate aaa
+pip3 install nltk
+pip3 install transformers==4.3.0
+pip3 install scikit-learn==0.24.1
+pip3 install packaging==21.3
+# pip3 install 'torchmetrics<0.8' # NOT SURE IF I NEED THIS
